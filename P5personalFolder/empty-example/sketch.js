846fe3cb61f40fa6cb89fcee9c5a227da69c0012
KeefersKeef <41490414+KeefersKeef@users.noreply.github.com>
2018-10-01 03:43:51 -0500

    I am trying to fix something i broke

--- P5personalFolder/empty-example/sketch.js
+++ P5personalFolder/empty-example/sketch.js
@@ -57,7 +57,7 @@ function puppyPic() {
 
 	background('blue');
 
-	image(dog,windowWidth/2 - 350, windowHeight/2 + 350, dog.width/2, dog.height/2);
+	image(dog,windowWidth/2 - 350, windowHeight/2 - 350, dog.width/5, dog.height/5);
 }
 
 function storyStart() {
